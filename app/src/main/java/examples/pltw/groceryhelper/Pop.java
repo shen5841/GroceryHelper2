5e4c59c6e23f1a96c8853035a98a530e8ff0b3a2
shen5841 <F2862089986@gmail.com>
2019-04-12 03:04:58 -0700

    grocery helper update 1.0.7 pop-up window & spinner

---
+++ app/src/main/java/examples/pltw/groceryhelper/Pop.java
@@ -0,0 +1,24 @@
+package examples.pltw.groceryhelper;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.support.annotation.Nullable;
+
+
+public class Pop extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.popwindow);
+
+        DisplayMetrics dm = new DisplayMetrics ();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int) (width*.8), (int) (height*.6));
+    }
+}
